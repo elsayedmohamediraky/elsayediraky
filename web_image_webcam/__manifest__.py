c89db62354c609d1ae2ab55e2e670f4bed23f49a
elsayediraky <elsayediraky330@gmail.com>
2020-02-07 04:32:41 +0200

    upgrade image webcam to odoo V12

---
+++ web_image_webcam/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Web Widget - Image WebCam",
+    "summary": "Allows to take image with WebCam",
+    "version": "12.0.1.0.0",
+    "category": "web",
+    "website": "https://www.eurodoo.com",
+    "author": "Tech Receptives, "
+              "Odoo Community Association (OCA), "
+              "Kaushal Prajapati",
+    "license": "LGPL-3",
+    "currency": "EUR",
+    "data": [
+        "views/assets.xml",
+    ],
+    "depends": [
+        "web",
+    ],
+    "qweb": [
+        "static/src/xml/web_widget_image_webcam.xml",
+    ],
+    "installable": True,
+}
