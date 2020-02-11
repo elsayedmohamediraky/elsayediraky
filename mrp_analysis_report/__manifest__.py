9b48ffce2e8d56d31fe8862de82b82625354945c
elsayediraky <elsayediraky330@gmail.com>
2020-02-11 14:50:11 +0200

    edit manfist file

--- mrp_analysis_report/__manifest__.py
+++ mrp_analysis_report/__manifest__.py
@@ -5,8 +5,13 @@
     'summary': """
         you Can From This Model Genrate Dynamic Reports From Manufacturing Orders Consumed martial and Finished , Scrap order By Product
           Contain From To Types Summery and Usage Variance and Detailed """,
+    "license": "AGPL-3",
+    'price': '100',
 
     'author': "El-Sayed Iraky",
+    'website': "https://www.linkedin.com/in/el-sayed-mohamed-0233ba134/",
+    'price': 80.00,
+    'currency': 'EUR',
     'category': 'MRP',
     'version': '0.1',
     'depends': ['base','mrp','stock','report_xlsx'],
